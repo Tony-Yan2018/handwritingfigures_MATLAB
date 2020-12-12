f94b774799eae5f80a8e2dff5817ac8793d25ce8
tonyYAN <yanwt2013@live.com>
2020-12-12 23:52:46 +0100

    updated classifier combination and code comments

--- probKNN.m
+++ probKNN.m
@@ -1,11 +1,12 @@
 % returns the probability vector of a test sample
 function y = probKNN(distArr,k)
 distArr = sort(distArr, 2); % ascending sort of distance array
-pickArr = zeros(1,10);% head pointer & class frequency indicator
+pickArr = zeros(1,10);% head pointer(the next item of a class) & class frequency indicator
 km=k;
 while k>0
     smallest = 100000;
     index = 0;
+    % traverse the first uncalculated element of each class (0-9)
     for i = 1:10
         current = distArr(i,pickArr(i)+1);
         if current<smallest
@@ -13,7 +14,7 @@ while k>0
             index = i;
         end
     end
-    pickArr(index) = pickArr(index)+1;
+    pickArr(index) = pickArr(index)+1; % index: class of the smallest (nearest) element after each iteration
     k=k-1;
 end
 y=pickArr/km;
